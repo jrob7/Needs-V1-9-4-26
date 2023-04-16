d942899571592e462453d3162de3e2ebb0fc1a92
Joshua Hurst <joshhurst@Joshs-MacBook-Pro.local>
2023-04-16 07:50:03 -0700

    Finished task 2 of assigntment one react native added contact information

--- Desktop/NucampFolder/4-React-Native/nucampsite/screens/ContactScreen.js
+++ Desktop/NucampFolder/4-React-Native/nucampsite/screens/ContactScreen.js
@@ -1,14 +1,26 @@
 import { ScrollView } from "react-native"
+import { Card, Text } from 'react-native-elements'
 
 const ContactScreen = () => {
 
 return (
 
 <ScrollView> 
-
-
-
-
+    <Card wrapperStyle={{margin: 20}}>
+         <Card.Title>
+         Contact Information
+         </Card.Title>
+         <Card.Divider/>
+         <Text style ={{marginBottom: 10}}>
+         1 Nucamp Way 
+         {'\n'} Seattle, WA 98001 
+         {'\n'} U.S.A.
+         </Text>
+         <Text>
+         Phone: 1-206-555-1234
+         Email: campsites@nucamp.co
+         </Text>
+     </Card>
 </ScrollView>
 
 )}

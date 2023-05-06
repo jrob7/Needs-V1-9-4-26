a8bf779cb8744c796a045a100e7962f8d81cb2cc
Joshua Hurst <joshhurst@joshs-mbp.lan>
2023-05-06 08:02:39 -0700

    finished task 1 by creating a new campsiteRouter.route with path  /:campsiteid

--- Desktop/NucampFolder/5-NodeJS-Express-MongoDB/node-express/routes/campsiteRouter.js
+++ Desktop/NucampFolder/5-NodeJS-Express-MongoDB/node-express/routes/campsiteRouter.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const campsiteRouter = express.Router();
 
-campsiteRouter.route('/')
-.all((req, res, next) => {
+campsiteRouter.route('/').all((req, res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     next();
@@ -21,4 +20,29 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+
+
+campsiteRouter.route('/:campsiteId').all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the campsites to you');
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+    res.end('Deleting all campsites');
+});
+
+
+
+
 module.exports = campsiteRouter;
+
